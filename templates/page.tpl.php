a7832da4cacb6d0f834f24a020346214287f1b8d
Evan Chute <echute@ginkgostreet.com>
2018-09-19 14:07:44 -0400

    hide the content area on the drupal homepage

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -146,7 +146,7 @@
             <?php if (theme_get_setting('breadcrumbs')): ?><?php if ($breadcrumb): ?><div id="breadcrumbs"><?php print $breadcrumb; ?></div><?php endif;?><?php endif; ?>
             <?php print $messages; ?>
             <?php if ($page['content_top']): ?><div id="content_top"><?php print render($page['content_top']); ?></div><?php endif; ?>
-            <div id="content-wrap">
+            <?php if (!drupal_is_front_page()): ?><div id="content-wrap">
               <?php print render($title_prefix); ?>
               <?php if ($title): ?><h1 class="page-title"><?php print $title; ?></h1><?php endif; ?>
               <?php print render($title_suffix); ?>
@@ -154,7 +154,7 @@
               <?php print render($page['help']); ?>
               <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
               <?php print render($page['content']); ?>
-            </div>
+            </div><?php endif; ?>
           </section>
         </div>
         <?php if ($page['sidebar_first']): ?>
